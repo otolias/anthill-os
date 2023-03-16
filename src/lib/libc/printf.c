3c596212b45330416ffdf0732647ebff15c74fea
p17anto2 <p17anto2@ionio.gr>
2023-03-17 01:41:01 +0200

    libc: Changed printf to allow system calls
    printf now sends to uart when its buffer is full.

--- src/lib/libc/printf.c
+++ src/lib/libc/printf.c
@@ -3,6 +3,34 @@
 #include <stdint.h>
 #include "uart.h"
 
+#define IO_BUFFER_SIZE     1024
+#define LOCAL_BUFFER_SIZE  64
+
+char io_buffer[IO_BUFFER_SIZE];
+uint32_t io_buffer_index = 0;
+
+/*
+* Send buffer contents to uart
+*/
+void flush_io_buffer() {
+    for (int i = 0; i < IO_BUFFER_SIZE; i++) {
+        uart_send_char(io_buffer[i]);
+        io_buffer[i] = '\x00';
+    }
+
+    io_buffer_index = 0;
+}
+
+/*
+* Append character to io_buffer. If the buffer is full, call flush_io_buffer
+*/
+void append_to_io_buffer(char c) {
+    if (io_buffer_index == IO_BUFFER_SIZE) 
+        flush_io_buffer();
+
+    io_buffer[io_buffer_index++] = c;
+}
+
 /*
 * Converts an int to a character array with base and writes it to the given buffer
 */
@@ -30,38 +58,38 @@ void printf(char *format, ...) {
     va_list va;
     char c;
     char *string;
-    char buffer[16];
+    char buffer[LOCAL_BUFFER_SIZE];
     uint32_t i = 0;
 
     va_start(va, format);
 
     while ((c = *(format++))) {
         if (c != '%') {
-            uart_send_char(c);
+            append_to_io_buffer(c);
         } else {
             c = *(format++);
 
             switch (c) {
                 case 'd':
                     integer_to_string(buffer, va_arg(va, uint32_t), 10);
-                    for (i = 0; i < 16; i++) {
+                    for (i = 0; i < LOCAL_BUFFER_SIZE; i++) {
                         if (!buffer[i]) break;
-                        uart_send_char(buffer[i]);
+                        append_to_io_buffer(buffer[i]);
                         buffer[i] = '\x00';
                     }
                     break;
                 case 'x':
                     integer_to_string(buffer, va_arg(va, uint32_t), 16);
-                    for (i = 0; i < 16; i++) {
+                    for (i = 0; i < LOCAL_BUFFER_SIZE; i++) {
                         if (!buffer[i]) break;
-                        uart_send_char(buffer[i]);
+                        append_to_io_buffer(buffer[i]);
                         buffer[i] = '\x00';
                     }
                     break;
                 case 's':
                     string = va_arg(va, char*);
                     while (*string) {
-                        uart_send_char(*string);
+                        append_to_io_buffer(*string);
                         string++;
                     }
                 default:
@@ -69,5 +97,8 @@ void printf(char *format, ...) {
             }
         }
     }
+
+    flush_io_buffer();        
+
     va_end(va);
 }
