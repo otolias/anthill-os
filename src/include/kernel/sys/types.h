afc5cff44f259fea25ae66c706e7ca45bf0e9def
p17anto2 <p17anto2@ionio.gr>
2024-02-21 23:47:00 +0200

    kernel: add definition guards on types

--- src/include/kernel/sys/types.h
+++ src/include/kernel/sys/types.h
@@ -1,10 +1,24 @@
 #ifndef _KERNEL_SYS_TYPES_H
 #define _KERNEL_SYS_TYPES_H
 
+#ifndef mode_t
 typedef int mode_t;
+#endif /* mode_t */
+
+#ifndef off_t
 typedef unsigned long off_t;
+#endif /* off_t */
+
+#ifndef pid_t
 typedef long pid_t;
+#endif /* pid_t */
+
+#ifndef size_t
 typedef unsigned long size_t;
+#endif /* size_t */
+
+#ifndef ssize_t
 typedef long ssize_t;
+#endif /* ssize_t */
 
 #endif /* _KERNEL_SYS_TYPES_H */
