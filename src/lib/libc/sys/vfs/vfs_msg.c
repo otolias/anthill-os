e3973605fc578a5496856bc942f98b85325a4960
tolias-an <p17anto2@ionio.gr>
2024-06-03 13:38:04 +0300

    libc: Changed vfs_msg_put to return 0 in case of error

--- src/lib/libc/sys/vfs/vfs_msg.c
+++ src/lib/libc/sys/vfs/vfs_msg.c
@@ -22,8 +22,10 @@ unsigned vfs_msg_put(struct vfs_msg *msg, char *buf) {
     unsigned len = fcall_msg_to_buf(&msg->fcall, (unsigned char *) buf,
                                     VFS_MAX_MSG_LEN - MQ_ID_SIZE);
 
-    if (len > 0)
-        memcpy(buf + len, &msg->mq_id, MQ_ID_SIZE);
+    if (len == 0)
+        return 0;
+
+    memcpy(buf + len, &msg->mq_id, MQ_ID_SIZE);
 
     return len + MQ_ID_SIZE;
 }
