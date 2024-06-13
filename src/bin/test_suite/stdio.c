f2ddb8332358cfe84a2879fea1cdd35014658ee1
tolias-an <p17anto2@ionio.gr>
2024-06-13 16:57:43 +0300

    bin: Updated stdio tests

--- src/bin/test_suite/stdio.c
+++ src/bin/test_suite/stdio.c
@@ -4,56 +4,79 @@
 #include <stdio.h>
 #include <string.h>
 
-static void _test_char(void) {
-    char buffer[16];
+static void _test_formatting(void) {
+    char buffer[128];
+    int res;
 
-    sprintf(buffer, "%c", 'c');
-    if (strcmp(buffer, "c") != 0)
+    res = sprintf(buffer, "%c", 'c');
+    if (res != 1 || strcmp(buffer, "c") != 0)
         puts("STDIO::ERROR::Character formatting failed");
-}
 
-static void _test_signed(void) {
-    char buffer[64];
+    res = sprintf(buffer, "%s %s", "String 1", "String 2");
+    if (res != 17 || strcmp(buffer, "String 1 String 2") != 0)
+        puts("STDIO::ERROR::String formatting failed");
 
-    sprintf(buffer, "%d", INT32_MAX);
-    if (strcmp(buffer, "2147483647") != 0)
+    res = sprintf(buffer, "%%");
+    if (res != 1 || strcmp(buffer, "%") != 0)
+        puts("STDIO::ERROR::Percent formatting escape failed");
+
+    res = sprintf(buffer, "%d", INT32_MAX);
+    if (res != 10 || strcmp(buffer, "2147483647") != 0)
         puts("STDIO::ERROR::Positive signed integer formatting failed");
 
-    sprintf(buffer, "%i", INT32_MIN + 1);
-    if (strcmp(buffer, "-2147483647") != 0)
+    res = sprintf(buffer, "%i", INT32_MIN + 1);
+    if (res != 11 || strcmp(buffer, "-2147483647") != 0)
         puts("STDIO::ERROR::Negative signed integer formatting failed");
 
-    sprintf(buffer, "%ld", INT64_MAX);
-    if (strcmp(buffer, "9223372036854775807") != 0)
+    res = sprintf(buffer, "%ld", INT64_MAX);
+    if (res != 19 || strcmp(buffer, "9223372036854775807") != 0)
         puts("STDIO::ERROR::Positive signed long formatting failed");
 
-    sprintf(buffer, "%ld", INT64_MIN + 1);
-    if (strcmp(buffer, "-9223372036854775807") != 0)
+    res = sprintf(buffer, "%ld", INT64_MIN + 1);
+    if (res != 20 || strcmp(buffer, "-9223372036854775807") != 0)
         puts("STDIO::ERROR::Negative signed long formatting failed");
-}
 
-static void _test_unsigned(void) {
-    char buffer[64];
-
-    sprintf(buffer, "%u", UINT32_MAX);
-    if (strcmp(buffer, "4294967295") != 0)
+    res = sprintf(buffer, "%u", UINT32_MAX);
+    if (res != 10 || strcmp(buffer, "4294967295") != 0)
         puts("STDIO::ERROR::Unsigned decimal integer formatting failed");
 
-    sprintf(buffer, "%x", UINT32_MAX);
-    if (strcmp(buffer, "ffffffff") != 0)
+    res = sprintf(buffer, "%x", UINT32_MAX);
+    if (res != 8 || strcmp(buffer, "ffffffff") != 0)
         puts("STDIO::ERROR::Unsigned hexadecimal integer formatting failed");
 
-    sprintf(buffer, "%lu", UINT64_MAX);
-    if (strcmp(buffer, "18446744073709551615") != 0)
+    res = sprintf(buffer, "%lu", UINT64_MAX);
+    if (res != 20 || strcmp(buffer, "18446744073709551615") != 0)
         puts("STDIO::ERROR::Unsigned decimal long formatting failed");
 
-    sprintf(buffer, "%lx", UINT64_MAX);
-    if (strcmp(buffer, "ffffffffffffffff") != 0)
+    res = sprintf(buffer, "%lx", UINT64_MAX);
+    if (res != 16 || strcmp(buffer, "ffffffffffffffff") != 0)
         puts("STDIO::ERROR::Unsigned hexadecimal long formatting failed");
 }
 
+static void _test_snprintf(void) {
+    char buffer[128];
+    int res;
+
+    res = snprintf(buffer, 128, "%s", "String");
+    if (res != 6 || memcmp(buffer, "String", 7) != 0)
+        puts("STDIO::ERROR::Snprintf nul termination failed");
+
+    res = snprintf(buffer, 0, "%s", "Another String");
+    if (res != 14 || memcmp(buffer, "Another String", 15) == 0)
+        puts("STDIO::ERROR::Snprintf n = 0 failed");
+}
+
+static void _test_sprintf(void) {
+    char buffer[128];
+    int res;
+
+    res = sprintf(buffer, "%s", "String");
+    if (res != 6 || memcmp(buffer, "String", 7) != 0)
+        puts("STDIO::ERROR::Sprintf nul termination failed");
+}
+
 void test_stdio(void) {
-    _test_char();
-    _test_signed();
-    _test_unsigned();
+    _test_formatting();
+    _test_snprintf();
+    _test_sprintf();
 }
