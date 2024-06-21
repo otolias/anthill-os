3886650d8fb5f0a1c9f24af94b4fc7cc72c6eaea
tolias-an <p17anto2@ionio.gr>
2024-06-21 14:17:38 +0300

    bin/hello: Reverted to simple hello world program

--- src/bin/hello/main.c
+++ src/bin/hello/main.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
 
 int main(void) {
-    FILE *f = fopen("/bin/hello", "r");
-    int c;
-    unsigned i = 0;
-
-    while ((c = fgetc(f)) != EOF) {
-        i++;
-    }
-
-    printf("Read 0x%x bytes\n", i);
+    puts("Hello, World!");
 }
