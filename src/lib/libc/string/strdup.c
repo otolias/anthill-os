a4a79f154989cb3ee3af1daa0726e415764ae98f
tolias-an <p17anto2@ionio.gr>
2024-06-28 13:59:21 +0300

    libc: Fixed strdup nul termination

--- src/lib/libc/string/strdup.c
+++ src/lib/libc/string/strdup.c
@@ -4,8 +4,8 @@
 #include <stdlib.h>
 
 char *strdup(const char *s) {
-    size_t size = strlen(s);
-    char *r = malloc(size + 1);
+    size_t size = strlen(s) + 1;
+    char *r = malloc(size);
     if (!r) return NULL;
 
     memcpy(r, s, size);
